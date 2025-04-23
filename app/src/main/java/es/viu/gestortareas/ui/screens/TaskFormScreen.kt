e5310f2071c5f9e799262583c748df261d94816e
pcl98 <pablo.cerdanlopez@gmail.com>
2025-04-23 20:46:50 +0200

    Creación de TaskFormScreen.kt

--- app/src/main/java/es/viu/gestortareas/ui/screens/TaskFormScreen.kt
+++ app/src/main/java/es/viu/gestortareas/ui/screens/TaskFormScreen.kt
@@ -1,9 +1,64 @@
 package es.viu.gestortareas.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import es.viu.gestortareas.ui.components.CustomTopBar
 
+/**
+ *
+ * Pantalla para crear o editar una tarea. Contiene campos de texto y botón de guardar.
+ *
+ */
 @Composable
-fun TaskFormScreen(navController: NavHostController) {
+fun TaskFormScreen(navController: NavController) {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = { CustomTopBar("Nueva Tarea") }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .padding(padding)
+        ) {
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Título") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Descripción") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+            Button(
+                onClick = { /* TODO: llamar a ViewModel */},
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("Guardar")
+            }
+        }
+    }
 
 }
